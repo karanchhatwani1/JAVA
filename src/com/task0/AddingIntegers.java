d0bc79704ca08839173a7027c24384efdd10480f
karanchhatwani1 <karanchh2001@gmail.com>
2021-03-26 13:25:46 +0530

    First Task

---
+++ src/com/task0/AddingIntegers.java
@@ -0,0 +1,17 @@
+package com.task0;
+
+import java.util.Scanner;
+
+public class AddingIntegers {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first Number");
+        a = sc.nextInt();
+        System.out.println("Enter Second Number");
+        b = sc.nextInt();
+
+        int c = a+b;
+        System.out.println("Addition of first and second number is : " + c);
+    }
+}
