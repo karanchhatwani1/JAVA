9f37bee3012e5b659764a5237f0f2af616ff4179
karanchhatwani1 <karanchh2001@gmail.com>
2021-03-26 13:03:05 +0530

    Initial commit

---
+++ src/com/task0/Main.java
@@ -0,0 +1,14 @@
+package com.task0;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+
+        System.out.println(a);
+    }
+}
