5b2c5fb95714f28d09867205e45839b19c8745e8
karanchhatwani1 <karanchh2001@gmail.com>
2021-03-26 22:43:02 +0530

    Thirteen Task

---
+++ src/com/task0/RoundFigure.java
@@ -0,0 +1,15 @@
+package com.task0;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class RoundFigure {
+    public static void main(String[] args) {
+        double num;
+        int n;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextDouble();
+
+        System.out.format("%.4f",num);
+    }
+}
