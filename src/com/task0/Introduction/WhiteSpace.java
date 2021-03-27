40559d1fd6a7fd653e19a60a91d0fe99bb56070a
karanchhatwani1 <karanchh2001@gmail.com>
2021-03-27 22:45:19 +0530

    Twelfth Task

--- src/com/task0/Introduction/WhiteSpace.java
+++ src/com/task0/Introduction/WhiteSpace.java
@@ -2,6 +2,7 @@ package com.task0.Introduction;
 
 public class WhiteSpace {
     public static void main(String[] args) {
+
         String s = "My Name is Karan";
         String str = s.replaceAll("\\s","");
         System.out.println("String with white spaces : "+s);
